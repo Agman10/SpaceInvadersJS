d307cee1b832bfbbaf92abac2254be0c595da2ed
august.moller <ag.man@live.se>
2019-09-20 15:50:28 +0200

    added code and comments

--- code.js
+++ code.js
@@ -14,12 +14,42 @@ class Sprite{
     this.width = this.currentSprite.width;
     this.height = this.currentSprite.height;
     }
+
+    render(){
+        ctx.drawImage(this.currentSprite, this.x, this.y);
+    }
+
+    importSprite(path, name){
+        var sprite = new Image;
+        sprite.src = path;
+        this.sprites[name] = sprite;
+    }
+
+    getSprite(name){
+        for(var sprite in this.sprites){
+            if(name == sprite){
+                return this.sprites[sprite];
+            }
+        }
+    }
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    move(x, y){
+        this.x+=x;
+        this.y+=y;
+    }
 }
+
 class Player{
 /*
 the class for the player. contains the life, collision,
 ability to shoot, player sprite
 */
+
 }
 
 class Invader{
@@ -27,6 +57,17 @@ class Invader{
 the class for the enemies. contains collision, moving pattern,
 shooting pattern, different enemy sprite
 */
+
+}
+
+class Shelter{
+/*
+This is the spot where you are hiding from the bullets.
+when a shelter gets hit it will turn more and more red untill it
+takes enough hits to make it dissapear, there will be 4 shelters
+in the game.
+*/
+
 }
 
 startGame()
