ea724c57b8c7c38f8a0669bc0fddc39cfa996ffc
august.moller <ag.man@live.se>
2019-09-27 13:16:15 +0200

    updated

--- code.js
+++ code.js
@@ -1,22 +1,24 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.rect(0, 0, 448, 512);
+/*
+ctx.rect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "black";
 ctx.fill();
+*/
 
 class Sprite{
     constructor(url, x, y){
     this.sprites = new Object();
     this.importSprite(url, "default");
     this.currentSprite = this.getSprite("default");
-    this.x = x
-    this.y = y
+    this.x = x;
+    this.y = y;
     this.width = this.currentSprite.width;
     this.height = this.currentSprite.height;
     }
 
     render(){
-        ctx.drawImage(this.currentSprite, this.x, this.y);
+        ctx.renderImage(this.currentSprite, this.x, this.y);
     }
 
     importSprite(path, name){
@@ -44,12 +46,16 @@ class Sprite{
     }
 }
 
-class Player{
+class Player{  
 /*
 the class for the player. contains the life, collision,
 ability to shoot, player sprite
 */
-
+playerSprite(){
+    var player = new Sprite(/* "sprites/player.png", 4, 4 */);
+    player.sprite = 'sprites/player.png'
+    ctx.drawImage(5, 5, player.sprite)
+    }
 }
 
 class Invader{
@@ -72,5 +78,9 @@ in the game.
 
 startGame()
 function startGame(){
-drawImage
+    player.setPosition(4, 4);
+    ctx.renderImage(5, 5, player.sprite)
+    player.render(3, 5)
+//drawImage
 }
+render();
