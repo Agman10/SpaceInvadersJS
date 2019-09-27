891c03b8439e4aa445b23a53b86c74eb07f8535d
august.moller <ag.man@live.se>
2019-09-27 15:35:47 +0200

    dont know what to say

--- code.js
+++ code.js
@@ -7,55 +7,42 @@ ctx.fill();
 */
 
 class Sprite{
-    constructor(url, x, y){
-    this.sprites = new Object();
-    this.importSprite(url, "default");
-    this.currentSprite = this.getSprite("default");
-    this.x = x;
-    this.y = y;
-    this.width = this.currentSprite.width;
-    this.height = this.currentSprite.height;
-    }
-
-    render(){
-        ctx.renderImage(this.currentSprite, this.x, this.y);
-    }
-
-    importSprite(path, name){
-        var sprite = new Image;
-        sprite.src = path;
-        this.sprites[name] = sprite;
-    }
-
-    getSprite(name){
-        for(var sprite in this.sprites){
-            if(name == sprite){
-                return this.sprites[sprite];
-            }
-        }
-    }
-
-    setPosition(x, y) {
+    /* constructor(url, x, y){
         this.x = x;
         this.y = y;
-    }
-
-    move(x, y){
-        this.x+=x;
-        this.y+=y;
-    }
+        this.importSprite(url, "default");
+        //this.width = this.currentSprite.width;
+        //this.height = this.currentSprite.height; */
 }
+
+var player = {
+    life: 3,
+    hit: false,
+    //position: (x, y),
+    shoot: false,
+    score: 0,
+    sprite: playersprite
+}
+
+var playersprite = new Image()
 
 class Player{  
 /*
 the class for the player. contains the life, collision,
 ability to shoot, player sprite
 */
-playerSprite(){
-    var player = new Sprite(/* "sprites/player.png", 4, 4 */);
-    player.sprite = 'sprites/player.png'
-    ctx.drawImage(5, 5, player.sprite)
-    }
+
+constructor(x, y){
+    player.sprite.src = 'sprites/player.png';
+    this.x = x
+    this.y = y
+}
+
+move(x, y){
+    this.x+=x;
+    this.y+=y;
+}
+
 }
 
 class Invader{
@@ -76,11 +63,65 @@ in the game.
 
 }
 
-startGame()
+
 function startGame(){
-    player.setPosition(4, 4);
-    ctx.renderImage(5, 5, player.sprite)
-    player.render(3, 5)
+    /* this.player.setPosition(4, 4);
+    ctx.renderImage(player.sprite)
+    player.render(3, 5) */
 //drawImage
 }
-render();
+startGame();
+/* render(); */
+drawImage(playersprite.src, 5, 6)
+console.log(player.life)
+
+
+
+
+
+
+
+/* player(setPosition){
+    this.player.setPosition(3, 4)
+}
+
+playerSprite(sprite,){
+    //var player = new Sprite( "sprites/player.png", 4, 4 );
+    this.player.sprite = 'sprites/player.png'
+    ctx.drawImage(sprite.src)
+    } */
+/*     constructor(url, x, y){
+    this.x = x;
+    this.y = y;
+    this.importSprite(url, "default");
+    //this.width = this.currentSprite.width;
+    //this.height = this.currentSprite.height;
+    }
+
+    render(){
+        ctx.renderImage(this.currentSprite, this.x, this.y);
+    }
+
+    importSprite(path, name){
+        var sprite = new Image();
+        sprite.src = sprite;
+        this.sprites[name] = sprite;
+    }
+
+     getSprite(name){
+        for(var sprite in this.sprites){
+            if(name == sprite){
+                return this.sprites[sprite];
+            }
+        }
+    } 
+
+    setPosition(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    move(x, y){
+        this.x+=x;
+        this.y+=y;
+    } */
